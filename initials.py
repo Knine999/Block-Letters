21e0b215055daed5834294b4f4945750561580d2
Knine999 <66857437+Knine999@users.noreply.github.com>
2024-12-02 20:18:52 -0500

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,11 @@
+#Iv Ro
+#I'm Funny Jaja
+for i in range(1, 8):
+    if i == 1:
+        print("IIIII  V   V")
+    elif i == 7:
+        print("IIIII    V")
+    elif i == 6:
+        print("  I     V V ")
+    else:
+        print("  I    V   V")
